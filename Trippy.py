a0cfa75ea02754b328c07f3f474fe5bde71e8747
CSA-RJU <33467710+CSA-RJU@users.noreply.github.com>
2018-10-25 23:37:15 -0500

    Add files via upload

--- Trippy.py
+++ Trippy.py
@@ -21,8 +21,7 @@ The purpose of this program in to
 write to a file the choices about
 where and how the user traveled.
 
-This Project was started on 10/15/18
-and was completed... UNKOWN.''') #Creates a label that says what's in the ".
+Version: 1.0.1''') #Creates a label that says what's in the ".
     aboL.grid(column=1, row=1, sticky="NSEW")
     EX.grid_columnconfigure(1,weight=1)
     EX.grid_rowconfigure(1,weight=1)
@@ -38,37 +37,20 @@ def QUIT(): #Quits.
    quit()
 
 def submit(*args): #Adds 34 percent to progressbar if hassent already, adds up price and displays it.
-##    if spinvar.get() == 1:
-##        spinvar.set("January")
-##    elif spinvar.get() == 2:
-##        spinvar.set("Febuary")
-##    elif spinvar.get() == 3:
-##        spinvar.set("March")
-##    elif spinvar.get() == 4:
-##        spinvar.set("April")
-##    elif spinvar.get() == 5:
-##        spinvar.set("May")
-##    elif spinvar.get() == 6:
-##        spinvar.set("June")
-##    elif spinvar.get() == 7:
-##        spinvar.set("July")
-##    elif spinvar.get() == 8:
-##        spinvar.set("August")
-##    elif spinvar.get() == 9:
-##        spinvar.set("September")
-##    elif spinvar.get() == 10:
-##        spinvar.set("October")
-##    elif spinvar.get() == 11:
-##        spinvar.set("November")
-##    elif spinvar.get() == 12:
-##        spinvar.set("December")
-##    else:
-##        print ("Please choose an option!")
-##        clear()
-    ledcheck=open("trip_ledge.txt", "a")
-    ledcheck.write("Country: " +str(couLB.get("2"))+ ", Month: " +str(spinvar.get())+ ", Mode of Travel: " +str(combovar.get())+ ", Notes: " +str(notT.get(1.0, END))) #Shows all applied data.
-    ledcheck.write("\n====================================================================================\n")  #For organising.
-    ledcheck.close()
+    if couLB.curselection()==():
+        print ("Please choose a country!")
+        clear()
+    elif spinvar.get()=="V-Month-V":
+        print ("Please choose a Month!")
+        clear()
+    elif combovar.get()=="V-Type of Travel-V":
+        print ("Please choose a Type of Travel!")
+        clear()
+    else:
+        ledcheck=open("trip_ledge.txt", "a")
+        ledcheck.write("Country: " +str(couLB.get(ACTIVE))+ ", Month: " +str(spinvar.get())+ ", Mode of Travel: " +str(combovar.get())+ ", Notes: " +str(notT.get(1.0, END))) #Shows all applied data.
+        ledcheck.write("\n====================================================================================\n")  #For organising.
+        ledcheck.close()
 
 ########################### Widgets ###########################
 
@@ -104,12 +86,12 @@ couSB.grid(column=11, row=20, sticky="NSEW")
 couLB['yscrollcommand']=couSB.set
 
 notL=Label(root, text="Notes:") #Creates a label that says what's in the ".
-notL.grid(column=20, row=19, sticky="NSEW")
+notL.grid(column=20, row=19, columnspan=30, sticky="NSEW")
 notT=Text(root, width=5, height=5)
 notT.grid(column=20, row=20, columnspan=30, sticky="NSEW")
 
 SB_list=["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-SB=Spinbox(root, values=SB_list, textvariable=spinvar)
+SB=Spinbox(root, state="readonly", values=SB_list, textvariable=spinvar)
 SB.grid(column=10, row=30, sticky="NSEW")
 spinvar.set("V-Month-V")
 
@@ -131,7 +113,7 @@ root.grid_rowconfigure(20,weight=1)
 root.grid_rowconfigure(30,weight=1)
 root.grid_rowconfigure(40,weight=1)
 
-root.title("@Trippy Bruh@") #Makes the title what is in the "s.
+root.title("Trip Logger") #Makes the title what is in the "s.
 root.config(menu=menubar) #Display the menu.
 root.mainloop()
 root.destroy()
